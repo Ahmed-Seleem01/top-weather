704e603ec6699d7b53461605ba35c10120d0cc50
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-03-12 21:50:14 +0200

    Rearrange functions in code and simplify some logic

--- src/index.js
+++ src/index.js
@@ -6,32 +6,11 @@ const DOMelements = (function () {
   return { container, form };
 })();
 
-  const renderWeatherData = function renderWeatherInfo(usedData) {
-    const createInfoElement = function (label, data) {
-      const elm = document.createElement("p");
-      elm.textContent = `${label}: ${data}`;
-      elm.classList.add("weather-data");
-      DOMelements.container.appendChild(elm);
-    };
-    console.log(usedData);
-    DOMelements.container.textContent = "";
-    createInfoElement("Temperature", usedData.main.temp);
-    createInfoElement("Feels like", usedData.main.feels_like);
-    createInfoElement("Humidity", usedData.main.humidity);
-    createInfoElement("main", usedData.weather.main);
-    createInfoElement("Description", usedData.weather.description);
-  };
-
-const userLocation = function getLocationFromUser() {
-  const location = document.querySelector("#location");
-  return { location: location.value };
-};
-
 const requestedWeatherData = (function () {
   const asyncGetLocation = async function asyncGetLocation(url) {
-    try {
-      const response = await fetch(url, { mode: "cors" });
-      const data = await response.json();
+      try {
+        const response = await fetch(url, { mode: "cors" });
+        const data = await response.json();
       const usedData = { weather: data.weather[0], main: data.main };
       renderWeatherData(usedData);
     } catch (error) {
@@ -41,14 +20,31 @@ const requestedWeatherData = (function () {
   const getLocationData = function getLocationData(e) {
     e.preventDefault();
     DOMelements.container.textContent = "";
+    const userLocation = document.querySelector("#location").value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${
-      userLocation().location
+      userLocation
     }&APPID=4cd54834209720f8529a795ba5933b01`;
     DOMelements.container.textContent = "waiting";
     asyncGetLocation(url);
   };
   return { getLocationData };
 })();
+
+const renderWeatherData = function renderWeatherInfo(usedData) {
+  const createInfoElement = function (label, data) {
+    const elm = document.createElement("p");
+    elm.textContent = `${label}: ${data}`;
+    elm.classList.add("weather-data");
+    DOMelements.container.appendChild(elm);
+  };
+  console.log(usedData);
+  DOMelements.container.textContent = "";
+  createInfoElement("Temperature", usedData.main.temp);
+  createInfoElement("Feels like", usedData.main.feels_like);
+  createInfoElement("Humidity", usedData.main.humidity);
+  createInfoElement("main", usedData.weather.main);
+  createInfoElement("Description", usedData.weather.description);
+};
 
 const screenController = function screenController() {
   const submitForm = function submitForm() {
