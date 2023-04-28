659347d1d8e9ec914d5de4135021d2dea0c1ba48
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-28 17:24:58 +0300

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// Importing normalize.css file to reset browser's default styles
 import "normalize.css";
+// Importing custom styles for the application
 import "./style.scss";
+// Importing the Background component
 import Background from "./components/Background";
-// writer a function for getting the location
+// Background("rain");
+
+// Declaring a function that will get the location data from the user
 const DOMelements = (function () {
   const container = document.querySelector(".render-data");
   const form = document.querySelector("form");
@@ -9,13 +14,18 @@ const DOMelements = (function () {
   return { container, form };
 })();
 
+// Declaring a function that will get the weather data from the API
 const requestedWeatherData = (function () {
   const asyncGetLocation = async function asyncGetLocation() {
     try {
+      // Getting the user's input for the location
       const userLocation = document.querySelector("#location").value;
+      // Creating a URL to access the OpenWeatherMap API
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&APPID=4cd54834209720f8529a795ba5933b01`;
+      // Fetching the data from the API
       const response = await fetch(url, { mode: "cors" });
       const data = await response.json();
+      // Extracting the required data from the API response
       const usedData = { weather: data.weather[0], main: data.main };
       return usedData;
     } catch (error) {
@@ -25,12 +35,13 @@ const requestedWeatherData = (function () {
 
   return { asyncGetLocation };
 })();
-
+// Declaring a function that will convert the temperature to Celsius
 function toCelsius(kelvinTemp) {
   const celsius = kelvinTemp - 273.15;
   return `${celsius.toFixed(2)} °C`;
 }
 
+// Declaring a function that will render the weather data on the screen
 const screenController = function screenController() {
   DOMelements.form.addEventListener("submit", getLocationData);
 
@@ -38,10 +49,13 @@ const screenController = function screenController() {
     e.preventDefault();
     // Display waiting while waiting for weather data to be ready
     DOMelements.container.textContent = "waiting";
+    // Getting the weather data from the API
     const usedData = await requestedWeatherData.asyncGetLocation();
+    // Rendering the weather data on the screen
     renderWeatherData(usedData);
   }
   const renderWeatherData = function renderWeatherInfo(usedData) {
+    // Declaring a function that will create a DOM element to display the weather data
     const createInfoElement = function (label, data) {
       const elm = document.createElement("p");
       elm.textContent = `${label}: ${data}`;
@@ -49,12 +63,14 @@ const screenController = function screenController() {
       DOMelements.container.appendChild(elm);
     };
     console.log(usedData);
+    // Clearing the container's content before rendering the weather data
     DOMelements.container.textContent = "";
+    // Creating and displaying the weather data on the screen
     createInfoElement("Temperature", toCelsius(usedData.main.temp));
     createInfoElement("Feels like", toCelsius(usedData.main.feels_like));
     createInfoElement("Humidity", `${usedData.main.humidity} %`);
     createInfoElement("Description", usedData.weather.description);
-
+    // Creating and displaying the weather icon on the screen
     const icon = document.createElement("img");
     icon.src = `https://openweathermap.org/img/wn/${usedData.weather.icon}@2x.png`;
     icon.alt = "weather icon";
@@ -65,4 +81,5 @@ const screenController = function screenController() {
   };
 };
 
+// Running the screenController function
 screenController();
