303052c8e929330d5498d550115568d2294281cf
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-28 17:27:23 +0300

    Add comments and update the images and its rendering

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,32 +1,29 @@
-import clear from "../images/grooveland-designs-zjoydJb17mE-unsplash.jpg";
-import clouds from "../images/stephanie-klepacki-gXG_2TpSBOc-unsplash.jpg";
-import rain from "../images/klim-musalimov-iDc5g9h-dME-unsplash.jpg";
-import storm from "../images/tasos-mansour-_hGPdpyMV-8-unsplash.jpg";
-import snow from "../images/adam-chang-IWenq-4JHqo-unsplash.jpg";
-import mist from "../images/dave-hoefler-od287vQyufw-unsplash.jpg";
+// Importing images for different weather backgrounds
+import clear from "../images/clear-sky.jpg";
+import clouds from "../images/clouds.jpg";
+import rain from "../images/rain.jpg";
+import storm from "../images/storm.jpg";
+import snow from "../images/snow.jpg";
+import mist from "../images/mist.jpg";
 
 /* eslint-disable quotes */
+// Define function to change background based on weather type
 const Background = (type) => {
   const container = document.querySelector("body");
-
+  // Check weather type and set background accordingly
   if (type.includes("clear")) {
-    container.style.background = `url(${clear})  no-repeat top fixed black`;
-  }
-  else if(type.includes("clouds")){
-    container.style.background = `url(${clouds})  no-repeat center fixed black`;
-  }
-  else if(type.includes("rain")){
-    container.style.background = `url(${rain})  no-repeat center fixed black`;
-  }
-  else if(type.includes("storm")){
-    container.style.background = `url(${storm})  no-repeat center fixed black`;
-  }
-  else if(type.includes("snow")){
-    container.style.background = `url(${snow})  no-repeat center fixed black`;
-  }
-  else if(type.includes("mist")){
-    container.style.background = `url(${mist})  no-repeat top fixed black`;
+    container.style.background = `url(${clear}) top fixed`;
+  } else if (type.includes("clouds")) {
+    container.style.background = `url(${clouds}) center`;
+  } else if (type.includes("rain")) {
+    container.style.background = `url(${rain}) center`;
+  } else if (type.includes("storm")) {
+    container.style.background = `url(${storm}) center`;
+  } else if (type.includes("snow")) {
+    container.style.background = `url(${snow}) center`;
+  } else if (type.includes("mist")) {
+    container.style.background = `url(${mist}) top`;
   }
 };
-
+// Export the Background function as default
 export default Background;
